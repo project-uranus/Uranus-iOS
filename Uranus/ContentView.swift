e76eb2068fee4f81d0232949809d21e05a5f8341
rudeigerc <rudeigerc@gmail.com>
2019-11-28 23:05:43 +0800

    Initial Commit

---
+++ Uranus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Uranus
+//
+//  Created by Yuchen Cheng on 2019/11/28.
+//  Copyright © 2019 Yuchen Cheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
